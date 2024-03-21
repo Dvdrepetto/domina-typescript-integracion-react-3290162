5fdb6f677dda6875ec8d13658044752bee82148b
Natalia Corea <46870371+nlunaty@users.noreply.github.com>
2024-03-21 20:35:11 +0100

    fix

--- domina-react-typescript/src/unidad05/leccion01/Unidad05Leccion01.tsx
+++ domina-react-typescript/src/unidad05/leccion01/Unidad05Leccion01.tsx
@@ -2,11 +2,10 @@ import React, { PropsWithChildren } from 'react';
 
 type PropsComponentePadre = {} & PropsWithChildren;
 
-// Componente que espera un solo hijo
 const ComponentePadre: React.FC<PropsComponentePadre> = ({ children }: PropsComponentePadre) => {
   return (
     <div>
-      {/* Se espera solo un hijo, pero se están pasando varios */}
+      <h5>{React.Children.count(children)}</h5>
       {children}
     </div>
   );
